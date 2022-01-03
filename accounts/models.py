ae27902b7f181f57185929e6d455f10463b7a620
jhs512 <jangka512@gmail.com>
2022-01-03 13:19:18 +0900

    ì유저 모델에 gender 칼럼 추가, 초 ENUM 칼럼

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,12 @@ from django.db import models
 
 
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MAN = "M", "남성"
+        WOMAN = "W", "여성"
+
     first_name = None
     last_name = None
     name = models.CharField('이름', max_length=100)
+    gender = models.CharField('성별', max_length=1, choices=GenderChoices.choices, blank=True)
     pass
